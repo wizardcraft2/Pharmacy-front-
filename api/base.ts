e5b50c5f239ffd53f100912c1e87647733c29a6f
wizardcraft2 <2th2hu22@gmail.com>
2025-03-18 09:54:36 -0700

    updating url

--- api/base.ts
+++ api/base.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: "https://pharmacy-backend-two.vercel.app",
+URL: "https://pharmacy-backend-7638.vercel.app/",
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000", // Replace with your API base URL
   timeout: 10000, // Timeout in milliseconds
   headers: {
